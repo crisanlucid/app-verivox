63318fc1bcb5ae9af8bbae262d8c7a10f197ac41
Lucian Crisan <crisan.lucid@yahoo.com>
2022-11-26 18:25:25 +0100

    [fix] Config project to support .env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
